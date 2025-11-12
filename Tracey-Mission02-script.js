5054fb5317bdd616e51fc1ac2ad6a574ae6fdd2b
Tracey Tomas <traceyt@missionreadyhq.com>
2025-11-12 13:08:12 +1300

    submission

--- Tracey-Mission02-script.js
+++ Tracey-Mission02-script.js
@@ -1,83 +1,86 @@
 const images = [
   {
+    title: "CARE LABELS",
     src: "images/understandingcarelabels.jpg",
-    description:
-      "Care Labels can be found on the inner left hand side of your garment",
+    description: `1/5 
+      Care Labels can be found on the inner left hand side of your garment.`,
   },
   {
+    title: "WOOLENS",
     src: "images/woolenscare.png",
-    description: "Woolens",
+    description: `2/5 
+    Gentle washing and drying tips for wool garments.`,
   },
   {
+    title: "WOVENS",
     src: "images/wovenscare.png",
-    description: "Wovens",
+    description: `3/5 
+    Key steps for cleaning various woven fabrics.`,
   },
   {
+    title: "OUTERWEAR",
     src: "images/outerwearcare.jpg",
-    description: "Outerwear",
+    description: `4/5 
+    Maintaining coats, jackets, and other outer layers.`,
   },
   {
+    title: "DENIM",
     src: "images/denimcare.png",
-    description: "Denim",
+    description: `5/5 
+    Extend life of jeans with these simple tips.`,
   },
 ];
 
 // set to images.length - 1, so 0 is the first image selected when right button is clicked
 let currentIndex = images.length - 1;
 
-// get elements by id for selected-Image, image-name, left-arrow and right arrow
-// get elements by query selector all for .thumbnail
-const selectedImage = document.getElementById("selected-image"); //element
+const imageTitle = document.getElementById("image-title"); // get elements by id
+const selectedImage = document.getElementById("selected-image");
 const imageDescription = document.getElementById("image-description");
 const leftArrow = document.getElementById("left-arrow");
 const rightArrow = document.getElementById("right-arrow");
-//                 selecting all elements in HTML with the class="thumbnail"
-const thumbnails = document.querySelectorAll(".thumbnail"); //[element, element, element]
+const thumbnails = document.querySelectorAll(".thumbnail"); //get elements by query selector all for .thumbnail [element, element, element]
 
-// to update the selected image and the displayed image name✨✨
+// to update the selected image and the displayed image name✨
 function updateGallery() {
+  imageTitle.innerText = images[currentIndex].title;
   selectedImage.src = images[currentIndex].src;
   imageDescription.innerText = images[currentIndex].description;
 }
 
 // to change current index to the next one
-// if at end of gallery, start from begining again
-// call update gallery
 function nextImage() {
   console.log("next image clicked");
   currentIndex++;
   if (currentIndex === images.length) {
-    currentIndex = 0;
+    currentIndex = 0; // if at end of gallery, start from beginning again
   }
   console.log(currentIndex);
-  updateGallery(); //✨✨ selectedImage.src = images[currentIndex].src  imageName.innerText = images[currentIndex].name
+  updateGallery(); //call update gallery✨
 }
 
 // to change current index to the previous one
-// if at beginning of gallery, change to end
-// call update gallery
 function prevImage() {
   console.log("previous image clicked");
-  currentIndex--;
   if (currentIndex === -1) {
-    currentIndex = images.length - 1;
+    currentIndex = images.length - 1; // if at beginning of gallery, change to end
   }
   console.log(currentIndex);
-  updateGallery(); //✨✨ selectedImage.src = images[currentIndex].src  imageName.innerText = images[currentIndex].name
+  updateGallery(); //call update gallery✨
+  currentIndex--;
 }
 
-// change current index to selected thumbnail index
-// call updateGallery
+// change current index to selected thumbnail index 👍
 function selectThumbnail(index) {
   currentIndex = index;
-  updateGallery(); //👍
+  updateGallery(); //call update gallery
 }
 
-// ADDING FUNCTIONS to elements by adding event listeners or directly in the html
+// ADDING FUNCTIONS TO ELEMENTS WITH EVENT LISTENERS
 leftArrow.addEventListener("click", prevImage);
 rightArrow.addEventListener("click", nextImage);
 thumbnails.forEach((thumbnail, index) => {
   thumbnail.addEventListener("click", () => {
-    selectThumbnail(index);
-  }); //function() SAME AS () =>
-}); //👍
+    selectThumbnail(index); //call select thumbnail👍
+  });
+});
